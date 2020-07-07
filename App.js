e007a04c57d35265e6c01c2bf484dd6f707692eb
Doni Setiawan <donisetiawan.050@gmail.com>
2020-07-07 10:47:41 +0700

    Making Fetch happen

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform, StatusBar,
+} from 'react-native';
+import Fetch from './app/fetch';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,5 +14,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    ...Platform.select({
+      ios: { paddingTop: 20 },
+      android: { paddingTop: StatusBar.currentHeight },
+    }),
   },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Open up App.js to start working on your app!
+      </Text>
+      <Fetch />
+      {/* eslint-disable-next-line react/style-prop-object */}
+    </View>
+  );
+}
---
+++ app/fetch/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+  },
+  text: {
+    alignSelf: 'center',
+    color: '#365899',
+  },
+});
+
+export default styles;
